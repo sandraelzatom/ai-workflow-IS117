a94000c8474b830ac27f22561947adb4e54ec068
sandraelzatom <sandraelzatom@gmail.com>
2026-05-06 23:54:22 -0400

    Enhance next.config.mjs with ESLint and TypeScript settings
    Added ESLint and TypeScript configuration options to ignore errors during builds.

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,12 @@ const nextConfig = {
   images: {
     unoptimized: true, // Required for GitHub Pages
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
